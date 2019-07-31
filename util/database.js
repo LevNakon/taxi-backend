5e2ca2e1a960302e7e61be82fe12a29efa366205
LevNakon <lionsoul887@gmail.com>
2019-07-31 17:09:13 +0300

    Sequelize Config

--- util/database.js
+++ util/database.js
@@ -1,9 +1,17 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('root', 'root', 'root',
-    {
-        host: 'localhost',
-        dialect: 'postgres'
-    });
+// const sequelize = new Sequelize('root', 'root', 'root',
+//     {
+//         host: 'localhost',
+//         dialect: 'postgres'
+//     });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
 
 module.exports = sequelize;
